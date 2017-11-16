635caf02b9d5203c7d2f583e8543b238a7de0b3c
alhad-lifeids <alhad@lifeids.com>
2017-11-16 15:28:50 +0530

    Create AbuseReportable.php

---
+++ src/Traits/AbuseReportable.php
@@ -0,0 +1,33 @@
+<?php namespace Lifeids\AbuseReportable\Traits;
+
+use Lifeids\AbuseReportable\Models\Report;
+use Illuminate\Database\Eloquent\Model;
+
+trait AbuseReportable
+{
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\MorphMany
+     */
+    public function abusereports()
+    {
+        return $this->morphMany(AbuseReport::class, 'abusereportable');
+    }
+
+    /**
+     * @param $data
+     * @param Model $reportable
+     *
+     * @return $this
+     */
+    public function abusereport($data, Model $abusereportable)
+    {
+        $abusereport = (new AbuseReport())->fill(array_merge($data, [
+            'abusereporter_id' => $abusereportable->id,
+            'reporter_type' => get_class($reportable),
+        ]));
+
+        $this->abusereports()->save($report);
+
+        return $abusereport;
+    }
+}
