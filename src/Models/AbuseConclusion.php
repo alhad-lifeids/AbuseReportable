772a1b4c61fd62e9cf1c83cd3c3737d3e2897103
alhad-lifeids <alhad@lifeids.com>
2017-11-16 15:33:45 +0530

    Create AbuseConclusion.php

---
+++ src/Models/AbuseConclusion.php
@@ -0,0 +1,37 @@
+<?php namespace Lifeids\AbuseReportable\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AbuseConclusion extends Model
+{
+    /**
+     * @var string
+     */
+    protected $table = 'abusereports_conclusions';
+
+    /**
+     * @var array
+     */
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    /**
+     * @var array
+     */
+    protected $casts = ['meta' => 'array'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function conclusion()
+    {
+        return $this->belongsTo(AbuseReport::class);
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\MorphTo
+     */
+    public function judge()
+    {
+        return $this->morphTo();
+    }
+}
